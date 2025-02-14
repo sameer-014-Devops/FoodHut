ce7635e5e77780f1865e578fd0c206efbee4eadb
Sameer A <sksameer4970@gmail.com>
2025-02-14 17:12:59 +0530

    Update FoodHut_Testing_Pipeline.groovy

--- Jenkins/FoodHut_Testing_Pipeline.groovy
+++ Jenkins/FoodHut_Testing_Pipeline.groovy
@@ -261,7 +261,7 @@ pipeline {
             steps{
                 sh """mkdir -p $Prod_Workspace"""
                 sh """cp -R Dockerfile *.yaml target $Prod_Workspace"""
-                echo '**********Copied the Dockerfile, foodhutdeploy.yaml, and target files to the Medicure Production workspace**********'
+                echo '**********Copied the Dockerfile, foodhutdeploy.yaml, and target files to the FoodHut Production workspace**********'
                 cleanWs()
                 deleteDir()
             }
